ed86e8bb23e12ac999b0f8cb39000f6356994451
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-18 21:54:51 +0900

    Aボタンで とまって、Bボタンで 0 に もどり、A＋Bボタンで再度カウントアップ。
    Added button functionalities to control counter behavior.

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
 input.onButtonPressed(Button.A, function () {
+    // Aボタンでカウントアップを停止
     isRunning = false
 })
-let isRunning = false
+input.onButtonPressed(Button.B, function () {
+    // Bボタンでカウンタを0に戻して表示（動作状態は変更しない）
+    counter = 0
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.AB, function () {
+    // A+B同時押しでカウンタを0に戻して再び��ウントを開始
+    counter = 0
+    isRunning = true
+    basic.showNumber(counter)
+})
+let isRunning = true
 let counter = 0
+counter = 0
 isRunning = true
 basic.forever(function () {
-    if (isRunning == true) {
+    if (isRunning) {
         counter += 1
         basic.showNumber(counter)
         basic.pause(1000)
